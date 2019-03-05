b54ddf5a1f7231b90ebdd9ed7c9fe0df1174ea0e
sf <fjkks5is@engs.tamagawa.ac.jp>
2019-03-05 09:40:19 +0000

    fix wall_stop

--- scripts/wall_stop.py
+++ scripts/wall_stop.py
@@ -28,6 +28,6 @@ if __name__ == '__main__':
 	rospy.init_node('wall_stop')
 	rospy.wait_for_service('/motor_on')
 	rospy.wait_for_service('/motor_off')
-	rospy.on_shutdown(rospy.ServiceProxy('/motor_off', Trigger).call())
+	rospy.on_shutdown(rospy.ServiceProxy('/motor_off', Trigger).call)
 	rospy.ServiceProxy('/motor_on', Trigger).call()
 	WallStop().run()
