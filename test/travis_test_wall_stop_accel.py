529f40c5d69c2122487af115fc39e26ede9e94b8
sf <fjkks5is@engs.tamagawa.ac.jp>
2019-03-06 07:31:49 +0000

    retry

--- test/travis_test_wall_stop_accel.py
+++ test/travis_test_wall_stop_accel.py
@@ -30,7 +30,7 @@ class WallStopAccelTest(unittest.TestCase):
         left, right = self.set_and_get(40,0,0,9)
         self.assertTrue(2000 < left == right ,"cannot stop")
 
-	left, right = self.set_and_get(40,5,5,0)
+	left, right = self.set_and_get(15,5,15,15)
 	self.assertTrue(left ==  right == 0,"cannot stop")
 
 if __name__ == '__main__':
